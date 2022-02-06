dae8aace7a0ffe9a606f47640321a090cb284e4a
Teddy Hwang <teddy@decount.co>
2022-02-06 16:05:54 -0500

    Added link to share button

--- src/lib/share.ts
+++ src/lib/share.ts
@@ -5,7 +5,8 @@ import { GAME_TITLE } from '../constants/strings'
 export const shareStatus = (guesses: string[], lost: boolean) => {
   navigator.clipboard.writeText(
     `${GAME_TITLE} ${solutionIndex} ${lost ? 'X' : guesses.length}/6\n\n` +
-      generateEmojiGrid(guesses)
+      generateEmojiGrid(guesses) +
+    `\n\nPlay at https://dummerdle.teddyh.io`
   )
 }
 
